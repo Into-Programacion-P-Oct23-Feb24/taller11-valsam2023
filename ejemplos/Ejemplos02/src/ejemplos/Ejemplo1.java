6adff7dc7ca7bdc63f8782ead3fb26486f8650b0
valsam2023 <mvsamaniego1@utpl.edu.ec>
2024-01-04 17:33:08 -0500

    Subir cambios

--- ejemplos/Ejemplos02/src/ejemplos/Ejemplo1.java
+++ ejemplos/Ejemplos02/src/ejemplos/Ejemplo1.java
@@ -34,11 +34,8 @@ public class Ejemplo1 {
     }
     
     public static double obtenerPromedioDatos(int [] datos){
-        int suma = 0;
+        int suma = obtenerSumaDatos(datos);
         double promedio;
-        for (int i = 0; i < datos.length; i++) {
-            suma = suma + datos[i];
-        }
         promedio = (double)suma/datos.length;
         return promedio;
     }
--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -9,13 +9,43 @@ package problemas;
  *
  * @author reroes
  */
+import java.util.Scanner;
+
 public class Problema2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("Ingrese 1 para obtener el area del Cuadrado, 2 para"
+                + " el area del Triangulo, 3 para el area del Rectangulo");
+        opcion = entrada.nextInt();
+
+        if (opcion == 1) {
+            obtenerAreaCuadrado();
+        } else {
+            if (opcion == 2) {
+                obtenerAreaTriangulo();
+            } else {
+                if (opcion == 3) {
+                    obtenerAreaRectangulo();
+                }
+            }
+        }
+        
+
     
+
+    public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la longitud del lado del triangulo");
+        double lado = entrada.nextDouble();
+        double area = lado * lado;
+        System.out.println("El area del cuadrado es: " + area);
+    }
 }
+
+
